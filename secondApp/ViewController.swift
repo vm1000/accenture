97abdb3d9b876cdb8d24b10ac3891b924d12b6c0
janis <jaanis.tima@gmail.com>
2019-04-02 14:22:24 +0300

    Initial Commit

---
+++ secondApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondApp
+//
+//  Created by egmars.janis.timma on 02/04/2019.
+//  Copyright © 2019 egmars.janis.timma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
